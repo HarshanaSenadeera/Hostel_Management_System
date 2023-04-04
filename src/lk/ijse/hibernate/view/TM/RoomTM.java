f37ac1f4c525f1af8ad5c214cfc0a2109c1168d8
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-04-04 09:35:32 +0530

    Add TM classes

---
+++ src/lk/ijse/hibernate/view/TM/RoomTM.java
@@ -0,0 +1,16 @@
+package lk.ijse.hibernate.view.TM;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomTM {
+
+    private String room_type_id;
+    private String type;
+    private String key_money;
+    private int qty;
+}
