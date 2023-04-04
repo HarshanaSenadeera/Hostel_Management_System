6224f6bae0a39859f7240db32a7859be82ff260f
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-04-04 23:08:29 +0530

    Add Appinizialior and create FXML forms

--- src/Appinizializor.java
+++ src/Appinizializor.java
@@ -1,6 +1,24 @@
-public class Appinizializor {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
+public class Appinizializor extends Application {
 
     public static void main(String[] args) {
 
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/lk/ijse/hibernate/view/Loging_Form.fxml")))));
+        primaryStage.centerOnScreen();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
     }
 }
